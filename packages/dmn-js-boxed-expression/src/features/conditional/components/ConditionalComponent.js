c785e1aa6be2fc07cdf1e4805b19579a900d8f23
pblumer <34165194+pblumer@users.noreply.github.com>
2026-09-09 00:16:23 +0200

    feat(boxed-expression): render DMN 1.5 conditionals

---
+++ packages/dmn-js-boxed-expression/src/features/conditional/components/ConditionalComponent.js
@@ -0,0 +1,46 @@
+import { is } from 'dmn-js-shared/lib/util/ModelUtil';
+
+
+export class ConditionalComponentProvider {
+  static $inject = [ 'components' ];
+
+  constructor(components) {
+    components.onGetComponent('expression', ({ expression }) => {
+      if (is(expression, 'dmn:Conditional')) {
+        return ConditionalComponent;
+      }
+    });
+  }
+}
+
+function ConditionalComponent({ expression }) {
+  const ifExpression = expression.get('if');
+  const thenExpression = expression.get('then');
+  const elseExpression = expression.get('else');
+
+  return (
+    <div className="conditional-expression">
+      <div className="conditional-if">
+        <Expression expression={ ifExpression } />
+      </div>
+      <div className="conditional-then">
+        <Expression expression={ thenExpression } />
+      </div>
+      <div className="conditional-else">
+        <Expression expression={ elseExpression } />
+      </div>
+    </div>
+  );
+}
+
+function Expression({ expression }, context) {
+  if (!expression) {
+    return null;
+  }
+
+  const Component = context.components.getComponent('expression', {
+    expression
+  });
+
+  return <Component expression={ expression } />;
+}
