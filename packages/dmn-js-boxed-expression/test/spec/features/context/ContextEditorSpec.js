dda5014839a992e7a6f7f5ab3b0a6bea21bc57a7
pblumer <34165194+pblumer@users.noreply.github.com>
2026-09-08 23:38:35 +0200

    test(boxed-expression): expose context re-import warnings

--- packages/dmn-js-boxed-expression/test/spec/features/context/ContextEditorSpec.js
+++ packages/dmn-js-boxed-expression/test/spec/features/context/ContextEditorSpec.js
@@ -22,8 +22,9 @@ describe('ContextEditor', function() {
     });
 
     const { warnings } = await editor.importXML(xml, { open: false });
+    const warningMessages = warnings.map(warning => warning.message).join('\n');
 
-    expect(warnings).to.have.lengthOf(0);
+    expect(warnings, warningMessages).to.have.lengthOf(0);
 
     const contextView = editor.getViews().find(
       view => view.id === 'Decision_Score'
