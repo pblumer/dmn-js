94208cdfce1890e7577dae2190b6435e086583a3
pblumer <34165194+pblumer@users.noreply.github.com>
2026-09-08 23:20:56 +0200

    test(boxed-expression): cover DMN 1.5 context editing

---
+++ packages/dmn-js-boxed-expression/test/spec/features/context/ContextEditorSpec.js
@@ -0,0 +1,104 @@
+import { expect } from 'chai';
+
+import TestContainer from 'mocha-test-container-support';
+
+import Editor from '../../../helper/Editor';
+
+import contextXML from '../../context.dmn';
+
+
+describe('ContextEditor', function() {
+
+  let testContainer;
+
+  beforeEach(function() {
+    testContainer = TestContainer.get(this);
+  });
+
+  async function createEditor(xml = contextXML) {
+    const editor = new Editor({
+      container: testContainer,
+      dmnVersion: '1.5'
+    });
+
+    const { warnings } = await editor.importXML(xml, { open: false });
+
+    expect(warnings).to.have.lengthOf(0);
+
+    const contextView = editor.getViews().find(
+      view => view.id === 'Decision_Score'
+    );
+
+    expect(contextView).to.exist;
+
+    await editor.open(contextView);
+
+    return editor;
+  }
+
+  it('should render editable context variable names', async function() {
+
+    // given
+    const editor = await createEditor();
+
+    // when
+    const variableInputs = testContainer.querySelectorAll(
+      '.context-entry-variable .dms-input'
+    );
+
+    // then
+    expect(variableInputs).to.have.lengthOf(2);
+    expect(variableInputs[0].value).to.eql('Base');
+    expect(variableInputs[1].value).to.eql('Bonus');
+
+    editor.destroy();
+  });
+
+
+  it('should update variable with undo/redo and save/re-import', async function() {
+
+    // given
+    const editor = await createEditor();
+    const activeViewer = editor.getActiveViewer();
+    const rootElement = activeViewer.getRootElement();
+    const contextExpression = rootElement.get('decisionLogic');
+    const firstEntry = contextExpression.get('contextEntry')[0];
+    const variable = firstEntry.get('variable');
+
+    const context = activeViewer.get('context');
+    const commandStack = activeViewer.get('commandStack');
+
+    // when
+    context.updateVariable(variable, { name: 'BaseAmount' });
+
+    // then
+    expect(variable.name).to.eql('BaseAmount');
+
+    // when
+    commandStack.undo();
+
+    // then
+    expect(variable.name).to.eql('Base');
+
+    // when
+    commandStack.redo();
+
+    // then
+    expect(variable.name).to.eql('BaseAmount');
+
+    // when
+    const { xml } = await editor.saveXML();
+
+    editor.destroy();
+
+    const reloadedEditor = await createEditor(xml);
+    const reloadedViewer = reloadedEditor.getActiveViewer();
+    const reloadedContext = reloadedViewer.getRootElement().get('decisionLogic');
+    const reloadedVariable = reloadedContext.get('contextEntry')[0].get('variable');
+
+    // then
+    expect(reloadedVariable.name).to.eql('BaseAmount');
+
+    reloadedEditor.destroy();
+  });
+});
