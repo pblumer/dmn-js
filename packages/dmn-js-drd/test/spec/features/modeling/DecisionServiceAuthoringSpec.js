71d03130d89f4bc4f4a375d17737be5d609f6ef7
pblumer <34165194+pblumer@users.noreply.github.com>
2026-09-11 11:02:01 +0200

    test(drd): cover Decision Service save reimport

--- packages/dmn-js-drd/test/spec/features/modeling/DecisionServiceAuthoringSpec.js
+++ packages/dmn-js-drd/test/spec/features/modeling/DecisionServiceAuthoringSpec.js
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 
 import TestContainer from 'mocha-test-container-support';
 
-import Modeler from 'src/Modeler';
+import DrdModeler from '../../../helper/DrdModeler';
 
 import decisionServiceAuthoringXML from '../../../fixtures/dmn/decision-service-authoring-15.dmn';
 
@@ -12,7 +12,7 @@ describe('features/modeling - DMN 1.5 Decision Service authoring', function() {
   let modeler;
 
   beforeEach(async function() {
-    modeler = new Modeler({
+    modeler = new DrdModeler({
       container: TestContainer.get(this),
       dmnVersion: '1.5'
     });
@@ -28,7 +28,7 @@ describe('features/modeling - DMN 1.5 Decision Service authoring', function() {
   });
 
 
-  it('should create and persist a Decision Service', function() {
+  it('should create and persist a Decision Service', async function() {
     const activeViewer = modeler.getActiveViewer();
     const canvas = activeViewer.get('canvas');
     const elementFactory = activeViewer.get('elementFactory');
@@ -45,12 +45,40 @@ describe('features/modeling - DMN 1.5 Decision Service authoring', function() {
     expect(shape.businessObject.$type).to.eql('dmn:DecisionService');
     expect(shape.businessObject.$parent).to.eql(root.businessObject);
     expect(shape.businessObject.di).to.exist;
-    expect(shape.businessObject.di.get('decisionServiceDividerLine')).to.exist;
+
+    const divider = shape.businessObject.di.get('decisionServiceDividerLine');
+    const waypoints = divider && divider.get('waypoint');
+    const dividerY = shape.y + Math.round(shape.height * 0.6);
+
+    expect(divider).to.exist;
+    expect(waypoints).to.have.lengthOf(2);
+    expect(waypoints[0].x).to.eql(shape.x);
+    expect(waypoints[0].y).to.eql(dividerY);
+    expect(waypoints[1].x).to.eql(shape.x + shape.width);
+    expect(waypoints[1].y).to.eql(dividerY);
 
     commandStack.undo();
     expect(shape.businessObject.$parent).to.not.exist;
 
     commandStack.redo();
     expect(shape.businessObject.$parent).to.eql(root.businessObject);
+
+    const { xml } = await modeler.saveXML({ format: true });
+
+    expect(xml).to.contain('<decisionService');
+    expect(xml).to.contain('DMNDecisionServiceDividerLine');
+
+    const { warnings } = await modeler.importXML(xml);
+    const warningMessages = warnings.map(warning => warning.message).join('\n');
+
+    expect(warnings, warningMessages).to.have.lengthOf(0);
+
+    const reimportedViewer = modeler.getActiveViewer();
+    const elementRegistry = reimportedViewer.get('elementRegistry');
+    const reimported = elementRegistry.get(shape.id);
+
+    expect(reimported).to.exist;
+    expect(reimported.businessObject.$type).to.eql('dmn:DecisionService');
+    expect(reimported.businessObject.di.get('decisionServiceDividerLine')).to.exist;
   });
 });
